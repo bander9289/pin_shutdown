a2346b183dc00dd4710ea45620800b2dfe2a4988
Brandon Anderson <bander9289+pub@gmail.com>
2015-03-18 12:15:05 -0500

    Wrap functionality in __main__

--- pin_shutdown/pin_shutdown.py
+++ pin_shutdown/pin_shutdown.py
@@ -1,25 +1,28 @@
-import RPi.GPIO as GPIO
-import time
-import os
+def __main__():
+  import RPi.GPIO as GPIO
+  import time
+  import os
+  
+  SHUTDOWN_GPIO = 18
+  
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(SHUTDOWN_GPIO, GPIO.IN)
+  
+  while True:
+      # block until a triggered to shutdown
+      GPIO.wait_for_edge(SHUTDOWN_GPIO, GPIO.FALLING)
+      
+      # shutdown now! (about to lose power)
+      if(GPIO.input(SHUTDOWN_GPIO) == False):
+      	print "Shutdown pin event! Shutdown in ~20 seconds ...."
+          os.system("sync")
+  	time.sleep(20)
+  
+      	if(GPIO.input(SHUTDOWN_GPIO) == False):
+              os.system("sync; sudo shutdown -h now")
+              break;
+  
+  	print "Canceling shutdown since ignition is back on"
 
-SHUTDOWN_GPIO = 18
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SHUTDOWN_GPIO, GPIO.IN)
-
-while True:
-    # block until a triggered to shutdown
-    GPIO.wait_for_edge(SHUTDOWN_GPIO, GPIO.FALLING)
-    
-    # shutdown now! (about to lose power)
-    if(GPIO.input(SHUTDOWN_GPIO) == False):
-    	print "Shutdown pin event! Shutdown in ~20 seconds ...."
-        os.system("sync")
-	time.sleep(20)
-
-    	if(GPIO.input(SHUTDOWN_GPIO) == False):
-            os.system("sync; sudo shutdown -h now")
-            break;
-
-	print "Canceling shutdown since ignition is back on"
-
+if __name__ == '__main__':
+  __main__()
